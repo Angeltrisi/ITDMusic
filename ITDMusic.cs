5be966f85386d4413ab4a08eaa703070fb96b172
queue <angeltrisix@gmail.com>
2024-10-22 15:46:31 -0500

    Add MusicDisplay support

--- ITDMusic.cs
+++ ITDMusic.cs
@@ -10,6 +10,18 @@ namespace ITDMusic
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class ITDMusic : Mod
 	{
-
-	}
+        public static ITDMusic Instance;
+        public ITDMusic() => Instance = this;
+        internal Mod musicDisplay = null;
+        public override void Load()
+        {
+            musicDisplay = null;
+            ModLoader.TryGetMod("MusicDisplay", out musicDisplay);
+        }
+        public override void Unload()
+        {
+            Instance = null;
+            musicDisplay = null;
+        }
+    }
 }
---
+++ Systems/MusicDisplaySupport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace ITDMusic.Systems
+{
+    public class MusicDisplaySupport : ModSystem
+    {
+        public override void PostSetupContent()
+        {
+            Mod musicDisplay = ITDMusic.Instance.musicDisplay;
+            if (musicDisplay is null) return;
+
+            void AddMusic(string songKey, string authorKey)
+            {
+                short slot = (short)MusicLoader.GetMusicSlot(Mod, "Music/" + songKey);
+                musicDisplay.Call("AddMusic", slot, Mod.GetLocalization("SongNames." + songKey), Mod.GetLocalization("AuthorNames." + authorKey), Mod.DisplayName);
+            }
+
+            AddMusic("BlueshroomGroves", "qAngel");
+            AddMusic("WanderingTheBlueshrooms", "Bluestarr");
+            AddMusic("DuneBearer", "Bluestarr");
+            AddMusic("DreadfulDunes", "Bluestarr");
+            AddMusic("InterstellarInvertebrate", "Bluestarr");
+            AddMusic("Gravekeeper", "Bluestarr");
+            AddMusic("WOMR", "Bluestarr");
+        }
+    }
+}
